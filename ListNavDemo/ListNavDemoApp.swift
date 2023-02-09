f457e70bcf62cb9e7468a9c63684da97c17ad9ac
DaraDaraV <devdaradara@gmail.com>
2023-02-09 11:30:42 +0900

    Initial Commit

---
+++ ListNavDemo/ListNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavDemoApp.swift
+//  ListNavDemo
+//
+//  Created by 류지예 on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct ListNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
